a79c48a7580c1a095c6d3f86fdfc31456209b17c
Fabian Burth <fabian.burth@sap.com>
2024-10-09 22:32:58 +0200

    initial untested implementation

--- pkg/ocm/configure.go
+++ pkg/ocm/configure.go
@@ -2,7 +2,12 @@ package ocm
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/fluxcd/pkg/runtime/conditions"
+	"k8s.io/apimachinery/pkg/types"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	"ocm.software/ocm/api/ocm"
 
@@ -30,6 +35,9 @@ func ConfigureOCMContext(ctx context.Context, r Reconciler, octx ocm.Context,
 
 	set := GetEffectiveConfigSet(ctx, obj, def)
 
+	// TODO: factor out the signing part - the required type assertion should
+	//   have been enough of a hint for that all along. This will allow to get
+	//   rid of the VerificationProvider interface.
 	var rerr rerror.ReconcileError
 	var signingkeys []Verification
 	if vprov, ok := obj.(v1alpha1.VerificationProvider); ok {
@@ -46,6 +54,42 @@ func ConfigureOCMContext(ctx context.Context, r Reconciler, octx ocm.Context,
 		status.MarkNotReady(r.GetEventRecorder(), obj, v1alpha1.ConfigureContextFailedReason, err.Error())
 
 		return rerror.AsNonRetryableError(fmt.Errorf("failed to configure ocm context: %w", err))
+	}
+
+	return nil
+}
+
+func ConfigureResolvers(ctx context.Context, client k8sclient.Client,
+	octx ocm.Context, resolvers []v1alpha1.Resolver) rerror.ReconcileError {
+
+	logger := log.FromContext(ctx)
+
+	for _, resolver := range resolvers {
+		resolverRepo := &v1alpha1.OCMRepository{}
+		if err := client.Get(ctx, types.NamespacedName{
+			Namespace: resolver.RepositoryRef.Namespace,
+			Name:      resolver.RepositoryRef.Name,
+		}, resolverRepo); err != nil {
+			logger.Info("failed to get resolver repository")
+
+			return rerror.AsRetryableError(fmt.Errorf("failed to get resolver repository: %w", err))
+		}
+
+		if resolverRepo.DeletionTimestamp != nil {
+			return rerror.AsNonRetryableError(errors.New("repository is being deleted, please do not use it"))
+		}
+
+		if !conditions.IsReady(resolverRepo) {
+			logger.Info("repository is not ready", "name", resolver.RepositoryRef.Name)
+
+			return rerror.AsRetryableError(errors.New("repository is not ready"))
+		}
+		resolverRepoSpec, err := octx.RepositorySpecForConfig(resolverRepo.Spec.RepositorySpec.Raw, nil)
+		if err != nil {
+			return rerror.AsNonRetryableError(fmt.Errorf("failed to unmarshal RepositorySpec: %w", err))
+		}
+
+		octx.AddResolverRule(resolver.Prefix, resolverRepoSpec, resolver.Priority)
 	}
 
 	return nil
