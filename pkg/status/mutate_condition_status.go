6edf07a68b9f846d5384398353d8dfbe163b1ceb
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2025-03-14 11:28:41 +0100

    fix: non-constant format string

--- pkg/status/mutate_condition_status.go
+++ pkg/status/mutate_condition_status.go
@@ -1,31 +1,25 @@
-// SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Open Component Model contributors.
-//
-// SPDX-License-Identifier: Apache-2.0
-
 package status
 
 import (
+	eventv1 "github.com/fluxcd/pkg/apis/event/v1beta1"
 	"github.com/fluxcd/pkg/apis/meta"
 	"github.com/fluxcd/pkg/runtime/conditions"
-
-	eventv1 "github.com/fluxcd/pkg/apis/event/v1beta1"
-	kuberecorder "k8s.io/client-go/tools/record"
-
 	"github.com/open-component-model/ocm-k8s-toolkit/pkg/event"
+	kuberecorder "k8s.io/client-go/tools/record"
 )
 
 // MarkNotReady sets the condition status of an Object to `Not Ready`.
 func MarkNotReady(recorder kuberecorder.EventRecorder, obj conditions.Setter, reason, msg string) {
 	conditions.Delete(obj, meta.ReconcilingCondition)
-	conditions.MarkFalse(obj, meta.ReadyCondition, reason, msg) //nolint:govet // Error is currently not relevant
+	conditions.MarkFalse(obj, meta.ReadyCondition, reason, "%s", msg)
 	event.New(recorder, obj, nil, eventv1.EventSeverityError, msg)
 }
 
 // MarkAsStalled sets the condition status of an Object to `Stalled`.
 func MarkAsStalled(recorder kuberecorder.EventRecorder, obj conditions.Setter, reason, msg string) {
 	conditions.Delete(obj, meta.ReconcilingCondition)
-	conditions.MarkFalse(obj, meta.ReadyCondition, reason, msg) //nolint:govet // Error is currently not relevant
-	conditions.MarkStalled(obj, reason, msg)                    //nolint:govet // Error is currently not relevant
+	conditions.MarkFalse(obj, meta.ReadyCondition, reason, "%s", msg)
+	conditions.MarkStalled(obj, reason, "%s", msg)
 	event.New(recorder, obj, nil, eventv1.EventSeverityError, msg)
 }
 
